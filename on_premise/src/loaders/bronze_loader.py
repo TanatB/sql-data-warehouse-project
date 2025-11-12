350a23d765c4a419846d40a4234d579a5e36f0c8
TanatB <bright.tanat@hotmail.com>
2025-11-12 15:43:25 +0700

    Fixed bronze_loader.py env variables, so now it can be connected to postgresql via psycopg2.

--- on_premise/src/loaders/bronze_loader.py
+++ on_premise/src/loaders/bronze_loader.py
@@ -4,11 +4,19 @@ from psycopg2.extras import Json
 import os
 from dotenv import load_dotenv
 from datetime import datetime, timezone
+from pathlib import Path
+
+PROJECT_ROOT = Path(__file__).parent.parent.parent.parent
+SQL_DIR = PROJECT_ROOT / "on_premise" / "sql"
+
+# Load environment variables from .env file
+load_dotenv()
 
 host = "localhost"
-db_name = os.getenv("POSTGRES_DB")
-user = os.getenv("POSTGRES_USER")
-password = os.getenv("POSTGRES_PASSWORD")
+port= os.getenv("POSTGRES_WAREHOUSE_PORT")
+db_name = os.getenv("POSTGRES_WAREHOUSE_DB")
+user = os.getenv("POSTGRES_WAREHOUSE_USER")
+password = os.getenv("POSTGRES_WAREHOUSE_PASSWORD")
 
 
 class SQLExecutor:
@@ -77,9 +85,6 @@ class SQLExecutor:
 
 
 if __name__ == "__main__":
-    # Load environment variables from .env file
-    load_dotenv()
-
     extractor = OpenMeteoExtractor(latitude=13.754, 
                 longitude=100.5014, 
                 location_name="Bangkok", 
@@ -89,12 +94,18 @@ if __name__ == "__main__":
                 "temperature_2m"
                 ])
     raw_response = extractor.extract_forecast_data()
+
+    print(type(raw_response))
     # print(raw_response)
 
-    # conn = psycopg2.connect(
-    #     host=host,
-    #     database=db_name,
-    #     user=user,
-    #     password=password
-    # )
-    # sql_executor = SQLExecutor(conn)
+    conn = psycopg2.connect(
+        host=host,
+        port=port,
+        dbname=db_name,
+        user=user,
+        password=password
+    )
+    sql_executor = SQLExecutor(conn)
+    # sql_executor.execute_file(SQL_DIR / "00_init" / "init_db.sql")
+    # sql_executor.execute_file(SQL_DIR / "01_bronze" / "bronze_layer.sql")
+    # sql_executor.insert_json(raw_response)
