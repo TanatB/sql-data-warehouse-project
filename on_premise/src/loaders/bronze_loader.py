af20ef185388dcf1f281203fb1803a19efd05969
TanatB <bright.tanat@hotmail.com>
2025-11-15 15:06:25 +0700

    bronze_loader is now ready to use for a production. The raw responses that has datetime() or bytes format are generally converted to string to serialize in JSON column.

--- on_premise/src/loaders/bronze_loader.py
+++ on_premise/src/loaders/bronze_loader.py
@@ -20,10 +20,26 @@ password = os.getenv("POSTGRES_WAREHOUSE_PASSWORD")
 
 
 class SQLExecutor:
+    """Test
+    Args:
+        conn ():
+    
+    Attributes:
+        conn ():
+    """
+
     def __init__(self, conn):
         self.conn = conn
 
     def execute_file(self, sql_file_path):
+        """
+        Recursively convert bytes to strings in nested dict/list structures.
+        
+        Args:
+            sql_file_path (str):
+        Returns:
+            bool: 
+        """
         with open(sql_file_path, 'r') as f:
             sql_script = f.read()
 
@@ -43,33 +59,57 @@ class SQLExecutor:
             cursor.close()
 
     def execute_directory(self, sql_dir, pattern="*.sql"):
+        """
+        test.
+        
+        Args:
+            sql_dir (str):
+            pattern (str):
+        Returns:
+            object: 
+        """
         pass
 
     def convert_to_json(self):
+        """
+        test.
+        
+        """
         pass
 
     def insert_json(self, data):
+        """
+        Recursively convert bytes to strings in nested dict/list structures.
+        
+        Args:
+            obj (object)
+        Returns:
+            object: 
+        """
         cursor = self.conn.cursor()
         insert_query = """--sql
                 INSERT INTO bronze.weather_raw (
-                latitude, longitude, api_retrieval_time, request_id, 
+                latitude, longitude, api_retrieval_time,
                 raw_response, response_time_ms, forecast_days, 
                 ingestion_timestamp, created_at) 
-                VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)
+                VALUES (%s, %s, %s, %s, %s, %s, %s, %s)
         """
 
         try:
+            cleaned_data = self._clean_format_in_dict(data)
+
             cursor.execute(insert_query, (
-                    data["api_response"]["latitude"],
-                    data["api_response"]["longitude"],
-                    data["metadata"]["api_retrieval_time"],
-                    Json(data),
-                    data["metadata"]["response_time_ms"],
+                    cleaned_data["api_response"]["latitude"],
+                    cleaned_data["api_response"]["longitude"],
+                    cleaned_data["metadata"]["api_retrieval_time"],
+                    Json(cleaned_data),
+                    cleaned_data["metadata"]["response_time_ms"],
                     7,
                     datetime.now(),
                     datetime.now()
             ))
 
+            self.conn.commit()
             print("successfully inserted the data.")
 
             return True
@@ -82,6 +122,27 @@ class SQLExecutor:
         
         finally:
             cursor.close()
+
+    def _clean_format_in_dict(self, obj):
+        """
+        Recursively convert bytes/datetime format to strings in nested dict/list.
+
+        Args:
+            obj (object):
+        
+        Returns:
+            obj:
+        """
+        if isinstance(obj, dict):
+            return {k: self._clean_format_in_dict(v) for k, v in obj.items()}
+        elif isinstance(obj, list):
+            return [self._clean_format_in_dict(item) for item in obj]
+        elif isinstance(obj, bytes):
+            return obj.decode('utf-8')
+        elif isinstance(obj, (datetime)):
+            return obj.isoformat()
+        else:
+            return obj
 
 
 if __name__ == "__main__":
@@ -108,4 +169,4 @@ if __name__ == "__main__":
     sql_executor = SQLExecutor(conn)
     # sql_executor.execute_file(SQL_DIR / "00_init" / "init_db.sql")
     # sql_executor.execute_file(SQL_DIR / "01_bronze" / "bronze_layer.sql")
-    # sql_executor.insert_json(raw_response)
+    sql_executor.insert_json(raw_response)
