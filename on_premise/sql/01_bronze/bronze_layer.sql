467bbc5892902d50c046cac7b05aa698bafb94d5
TanatB <bright.tanat@hotmail.com>
2025-11-19 15:07:42 +0700

    Added api_error_log & error index on bronze_layer.sql

--- on_premise/sql/01_bronze/bronze_layer.sql
+++ on_premise/sql/01_bronze/bronze_layer.sql
@@ -1,29 +1,37 @@
 
 -- TABLE
-CREATE TABLE IF NOT EXISTS bronze.weather_raw(
+CREATE TABLE IF NOT EXISTS bronze.weather_raw (
     record_id SERIAL PRIMARY KEY,
-    
-    latitude DECIMAL(10, 6) NOT NULL,
-    longitude DECIMAL(10,6) NOT NULL,
+
+    -- retrieval_time is recorded When API was called
     api_retrieval_time TIMESTAMP NOT NULL,
-    request_id INTEGER,
+    response_time_ms INTEGER,
+
+    -- AUDIT, when record was created, for future batch processing
+    created_at TIMESTAMP DEFAULT NOW(),
 
     -- API RESPONSE
-    raw_response JSONB,
-
-    -- METADATA
-    response_time_ms INTEGER,
-    forecast_days INTEGER,
-    
-    -- AUDIT
-    ingestion_timestamp TIMESTAMP DEFAULT NOW(),
-    created_at TIMESTAMP DEFAULT NOW()
+    raw_api_response JSONB NOT NULL
 );
 
+
+-- For insertion failure tracking
+CREATE TABLE IF NOT EXISTS bronze.api_error_log (
+    error_id SERIAL PRIMARY KEY,
+    error_timestamp TIMESTAMP DEFAULT NOW(),
+    api_endpoint VARCHAR(500),
+    error_type VARCHAR(100),
+    error_message TEXT,
+    response_status_code INTEGER,
+    request_params JSONB
+);
+
+
 -- INDEX
-CREATE INDEX idx_weather_raw_location 
-    ON bronze.weather_raw (latitude, longitude);
-CREATE INDEX idx_weather_raw_api_response_gin 
-    ON bronze.weather_raw (raw_response);
-CREATE INDEX idx_weather_raw_location_time
-    ON bronze.weather_raw (latitude, longitude, created_at DESC)
+CREATE INDEX idx_weather_raw_record_id
+    ON bronze.weather_raw (record_id);
+CREATE INDEX idx_weather_raw_api_retrieval_time
+    ON bronze.weather_raw (api_retrieval_time);
+
+CREATE INDEX idx_error_timestamp 
+    ON bronze.api_error_log(error_timestamp DESC);
