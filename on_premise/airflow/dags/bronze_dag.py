85c6c8c830e622e9289e6fac3a079c5dcf5b2d9b
TanatB <bright.tanat@hotmail.com>
2026-01-22 20:45:52 +0700

    Added docstring and added more hourly metrics for silver layer transformation preparation.

--- on_premise/airflow/dags/bronze_dag.py
+++ on_premise/airflow/dags/bronze_dag.py
@@ -1,6 +1,8 @@
 from airflow.sdk import dag, task
+from airflow.datasets import Dataset
 from airflow.providers.standard.operators.bash import BashOperator
 from airflow.providers.standard.operators.python import PythonOperator
+
 import psycopg2
 
 import os, logging
@@ -11,21 +13,21 @@ logger = logging.getLogger(__name__)
 
 LOCATIONS = [
     {
-        "name": "Bangkok",
+        "location_name": "Bangkok",
         "latitude": 13.754,
         "longitude": 100.5014,
         "timezone": "Asia/Bangkok"
     },
 
     {
-        "name": "Tokyo",
+        "location_name": "Tokyo",
         "latitude": 35.6762,
         "longitude": 139.6503,
         "timezone": "Asia/Tokyo"
     },
 
     {
-        "name": "Paris",
+        "location_name": "Paris",
         "latitude": 48.8534,
         "longitude": 2.3488,
         "timezone": "auto"
@@ -41,6 +43,11 @@ HOURLY_VARIABLES = [
     "weather_code",
     "wind_speed_10m",
     "wind_direction_10m",
+    "uv_index",
+    "is_day",
+    "rain",
+    "showers",
+    "snowfall"
 ]
 
 
@@ -56,6 +63,8 @@ default_args = {
     'max_retry_delay': timedelta(minutes = 30),
 }
 
+# URI is just a convention (can be anything) but I use this for self-documenting
+bronze_dataset = Dataset("postgres://data_warehouse/bronze.weather_raw")
 
 @dag(
     dag_id = 'weather_extraction_daily',
@@ -70,7 +79,7 @@ default_args = {
 def weather_etl_pipeline():
     """Weather ETL Pipeline using TaskFlow API.
 
-    Extract -> Load Bronze -> Transform to Silver (in progress)
+    Extract -> Load Bronze -> Transform to Silver (Later in the new script)
     """
 
     @task(retries=3, retry_delay=timedelta(minutes = 2))
@@ -79,7 +88,7 @@ def weather_etl_pipeline():
         Extract weather data from Open-Meteo API.
 
         Args:
-            location_config (dict): latitude, longtitude, location_name, timezon
+            location_config (dict): latitude, longtitude, location_name, timezone
 
         Returns:
             dict: api_response & metadata
@@ -87,7 +96,7 @@ def weather_etl_pipeline():
         from weather_etl.extractors.open_meteo_api_extractor import OpenMeteoExtractor
         
         logger.info("Starting weather extraction")
-        retry_attempt = context["task_instance"].try_number - 1
+        retry_attempt = context["task_instance"].try_number - 1 # convert to zero-indexed
 
         extractor = OpenMeteoExtractor(
             latitude = location_config["latitude"],
@@ -109,8 +118,17 @@ def weather_etl_pipeline():
             "location_name" : location_config["location_name"]
         }
 
-    @task
+    @task(outlets=[bronze_dataset])
     def load_city_to_bronze(extracted_data: dict) -> dict:
+        """
+        Use SQLExecutor class to connect psycopg2 to execute scripts locally.
+
+        Args:
+            extracted_data (dict): api_response, metadata, location_name
+
+        Returns:
+            dict: success (success status), location (location name)
+        """
         from weather_etl.loaders.bronze_loader import SQLExecutor
 
         conn = psycopg2.connect(
@@ -141,10 +159,11 @@ def weather_etl_pipeline():
     
     @task()
     def log_pipeline_result(load_result: dict):
-        """_summary_
+        """
+        Log the Pipeline result using logging module into PostgreSQL database for debugging.
 
         Args:
-            load_result (dict): _description_
+            load_result (dict): success (success status), location (location name)
         """
         import logging
 
@@ -153,20 +172,9 @@ def weather_etl_pipeline():
         else:
             logging.error(f"Pipeline failed for {load_result['location']}")
 
-    bangkok_config = {
-        "latitude": 13.754,
-        "longitude": 100.5014,
-        "location_name": "Bangkok",
-        "timezone": "Asia/Bangkok",
-        "hourly_variables": [
-            "temperature_2m",
-            "apparent_temperature",
-            "relative_humidity_2m",
-            "precipitation",
-            "precipitation_probability",
-            "weather_code",
-        ]
-    }
+    # Specify one location (Bangkok) for now, may implement multi-city later
+    bangkok_config = LOCATIONS[0]
+    bangkok_config['hourly_variables'] = HOURLY_VARIABLES
 
     extracted = extract_city_weather_data(bangkok_config)
     loaded = load_city_to_bronze(extracted)
