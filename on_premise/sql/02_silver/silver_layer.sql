74d645f5c3463c0bccd1147da6f31422bb090d6a
TanatB <bright.tanat@hotmail.com>
2026-01-23 14:31:42 +0700

    Added JSONB array extraction using UNNEST() SELECTION (not quite optimized yet).

--- on_premise/sql/02_silver/silver_layer.sql
+++ on_premise/sql/02_silver/silver_layer.sql
@@ -1,3 +1,5 @@
+CREATE SCHEMA IF NOT EXISTS silver;
+
 CREATE TABLE IF NOT EXISTS silver.weather_observations (
     id SERIAL PRIMARY KEY,
 
@@ -84,62 +86,43 @@ INSERT INTO silver.weather_observations (
     showers_mm,
     snowfall_mm,
     api_retrieval_time
-
-);
-
-
--- EDA
-
-SELECT raw_api_response -> 'hourly'
-FROM bronze.weather_raw;
-
-SELECT 
-    record_id,
-    pg_typeof(raw_api_response) as actual_type,
-    raw_api_response
-FROM bronze.weather_raw
-LIMIT 1;
-
--- See how the column was defined
-SELECT 
-    column_name, 
-    data_type, 
-    udt_name
-FROM information_schema.columns 
-WHERE table_schema = 'bronze' 
-  AND table_name = 'weather_raw';
-
--- List top-level keys
-SELECT DISTINCT jsonb_object_keys(raw_api_response) as top_level_keys
-FROM bronze.weather_raw;
-
--- List top-level keys from hourly variable
-SELECT DISTINCT jsonb_object_keys(raw_api_response -> 'hourly') as hourly_level_keys
-FROM bronze.weather_raw;
-
-
+)
 SELECT
-    record_id as bronze_record_id,
-    (raw_api_response ->> 'latitude')::FLOAT as latitude,
-    (raw_api_response ->> 'longitude')::FLOAT as longitude,
-    time_value::TIMESTAMPTZ as observation_timestamp,
-    temp_value::FLOAT as temperature_2m_celsius,
-    apparent_temp_value::FLOAT as apparent_temperature_celsius,
-    humidity_value::FLOAT as relative_humidity_2m_percent,
-    precipitation_value::FLOAT as precipitation_mm,
-    weather_code_value as weather_code,
-    is_day_value as is_day,
-    wind_speed_value::FLOAT as wind_speed_10m_kmh
-FROM bronze.weather_raw,
-     UNNEST(
-         ARRAY(SELECT jsonb_array_elements_text(raw_api_response -> 'hourly' -> 'time')),
-         ARRAY(SELECT jsonb_array_elements_text(raw_api_response -> 'hourly' -> 'temperature_2m')),
-         ARRAY(SELECT jsonb_array_elements_text(raw_api_response -> 'hourly' -> 'apparent_temperature')),
-         ARRAY(SELECT jsonb_array_elements_text(raw_api_response -> 'hourly' -> 'relative_humidity_2m')),
-         ARRAY(SELECT jsonb_array_elements_text(raw_api_response -> 'hourly' -> 'precipitation')),
-         ARRAY(SELECT jsonb_array_elements_text(raw_api_response -> 'hourly' -> 'weather_code')),
-         ARRAY(SELECT jsonb_array_elements_text(raw_api_response -> 'hourly' -> 'is_day')),
-         ARRAY(SELECT jsonb_array_elements_text(raw_api_response -> 'hourly' -> 'wind_speed_10m'))
-     ) AS t(time_value, temp_value, apparent_temp_value, humidity_value, precipitation_value, weather_code_value, is_day_value, wind_speed_value)
-WHERE is_day_value IS NOT NULL
-LIMIT 100;
+    w.record_id as bronze_record_id,
+    (w.raw_api_response->>'latitude')::FLOAT as latitude,
+    (w.raw_api_response->>'longitude')::FLOAT as longitude,
+    w.raw_api_response->>'timezone' as timezone,
+    time_val::TIMESTAMPTZ as observation_timestamp,
+    temp_val::FLOAT as temperature_2m_celsius,
+    apparent_temp_val::FLOAT as apparent_temperature_celsius,
+    humidity_val::FLOAT as relative_humidity_2m_percent,
+    precip_val::FLOAT as precipitation_mm,
+    weather_code_val as weather_code,
+    is_day_val::BOOLEAN as is_day,
+    wind_speed_val::FLOAT as wind_speed_10m_kmh,
+    cloud_cover_val::FLOAT as cloud_cover_percent,
+    uv_val::FLOAT as uv_index,
+    rain_val::FLOAT as rain_mm,
+    showers_val::FLOAT as showers_mm,
+    snowfall_val::FLOAT as snowfall_mm,
+    w.api_retrieval_time as api_retrieval_time
+FROM bronze.weather_raw w,
+UNNEST(
+    -- Extract each array from the JSONB
+    ARRAY(SELECT jsonb_array_elements_text(w.raw_api_response->'hourly'->'time')),
+    ARRAY(SELECT jsonb_array_elements_text(w.raw_api_response->'hourly'->'temperature_2m')),
+    ARRAY(SELECT jsonb_array_elements_text(w.raw_api_response->'hourly'->'apparent_temperature')),
+    ARRAY(SELECT jsonb_array_elements_text(w.raw_api_response->'hourly'->'relative_humidity_2m')),
+    ARRAY(SELECT jsonb_array_elements_text(w.raw_api_response->'hourly'->'precipitation')),
+    ARRAY(SELECT jsonb_array_elements_text(w.raw_api_response->'hourly'->'weather_code')),
+    ARRAY(SELECT jsonb_array_elements_text(w.raw_api_response->'hourly'->'is_day')),
+    ARRAY(SELECT jsonb_array_elements_text(w.raw_api_response->'hourly'->'wind_speed_10m')),
+    ARRAY(SELECT jsonb_array_elements_text(w.raw_api_response->'hourly'->'cloud_cover')),
+    ARRAY(SELECT jsonb_array_elements_text(w.raw_api_response->'hourly'->'rain')),
+    ARRAY(SELECT jsonb_array_elements_text(w.raw_api_response->'hourly'->'showers')),
+    ARRAY(SELECT jsonb_array_elements_text(w.raw_api_response->'hourly'->'snowfall')),
+    ARRAY(SELECT jsonb_array_elements_text(w.raw_api_response->'hourly'->'uv_index'))
+) AS h(time_val, temp_val, apparent_temp_val, humidity_val, precip_val, weather_code_val, is_day_val, wind_speed_val, cloud_cover_val, rain_val, showers_val, snowfall_val, uv_val)
+WHERE w.created_at > NOW() - INTERVAL '1 hour'
+ON CONFLICT (latitude, longitude, observation_timestamp) 
+DO NOTHING;
